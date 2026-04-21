69f3230db91b3823cc935efb5e72b29c8e5aed30
Janika,Rahikainen <ag3081@student.jamk.fi>
2026-04-21 15:35:54 +0300

    backendin alustus

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-// Elastic Beanstalk käyttää oletuksena porttia 8080
+const mongoose = require('mongoose');
+
 app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log('Connected to MongoDB Atlas!'))
+  .catch((err) => console.error('Database error ->', err));
 
 const port = process.env.PORT || 8080;
 app.get('/', (req, res) => {
---
+++ backend/models/test.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const TestSchema = new mongoose.Schema({
+  tervehdys: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model('Test', TestSchema);
